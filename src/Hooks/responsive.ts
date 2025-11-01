12464d85bb256d6877aa749da54bc45ed51480c1
Anish Dhar <106864236+Anishbwu123@users.noreply.github.com>
2025-11-01 21:20:50 +0530

    Implement responsive styles in container and text
    Added responsive styles for various UI components.

--- src/Hooks/responsive.ts
+++ src/Hooks/responsive.ts
@@ -33,3 +33,52 @@ const responsive: Responsive = {
 };
 
 export default responsive;
+
+
+
+
+
+------------usage-----------
+
+  const styles = StyleSheet.create({
+  box: {flex: 1},
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    padding: responsive.padding(10),
+  },
+  headerText: {
+    fontSize: responsive.fontSize(22),
+    fontWeight: 'bold',
+    marginBottom: responsive.margin(20),
+    textAlign: 'center',
+  },
+  bannerImage: {
+    width: '94%',
+    alignSelf: 'center',
+    height: responsive.height(150),
+    borderRadius: responsive.borderRadius(10),
+    marginBottom: responsive.margin(20),
+  },
+  sectionHeading: {
+    fontSize: responsive.fontSize(18),
+    fontWeight: 'bold',
+    marginBottom: responsive.margin(10),
+    marginTop: responsive.margin(20),
+  },
+  categoriesContainer: {
+    marginBottom: responsive.margin(20),
+  },
+  categoryItem: {
+    backgroundColor: '#007bff',
+    paddingVertical: responsive.padding(8),
+    paddingHorizontal: responsive.padding(15),
+    marginRight: responsive.margin(10),
+    borderRadius: responsive.borderRadius(10),
+  },
+  categoryText: {
+    fontSize: responsive.fontSize(14),
+    color: '#fff',
+  },
+  trendingItemsContainer: {
+    marginBottom: responsive.margin(20),
